faab45078de1994b86be86cafa425c431aada344
LonwaboMvovo <44nator@gmail.com>
2020-06-02 12:21:28 +0200

    Initial working project

--- main.py
+++ main.py
@@ -1,17 +1,23 @@
-from math import ceil
+# Total and tip calculator for a given bill, tip percentage and number of people rounded to the chosen rounding
+# 01/06/2020
+# Lonwabo Mvovo
 
-currency = input('Currency: ')
-bill = float(input('Bill Amount: '))
-tip_percent = float(input('Tip Percent: '))
-num_ppl = int(input('How many people to divide by: '))
-round_up = input(f'Round to nearest {currency} (y)es or (n)o: ')
+num_ppl = int(input('Number of people: '))
+bill = float(input('Bill: '))
+tip_perc = float(input('Tip percentage: '))
+tip = bill * (tip_perc/100)
+total = bill + tip
 
-tip = bill * (tip_percent / 100)
-total = bill + bill * (tip_percent / 100)
-tip_pp = tip / num_ppl
-total_pp = total / num_ppl
+print(f'Tip: {round(tip, 2):.2f}')
+print(f'Total: {round(total, 2):.2f}')
 
-print(f'\nTotal: {currency}{ceil(total) if round_up == "y" else round(total, 2)}')
-print(f'Total (per person): {currency}{ceil(total_pp) if round_up == "y" else round(total_pp, 2)}')
-print(f'Tip: {currency}{ceil(tip) if round_up == "y" else round(tip, 2)}')
-print(f'Tip (per person): {currency}{ceil(tip_pp) if round_up == "y" else round(tip_pp, 2)}')
+if num_ppl > 1:
+    tip_pp = tip / num_ppl
+    total_pp = total / num_ppl 
+
+    print(f'Tip per person: {round(tip_pp, 2):.2f}')
+    print(f'Total per person: {round(total_pp, 2):.2f}')
+
+    short = total - round(total_pp, 2) * 3
+    if short > 0:
+        print(f'Short: {round(short, 2):.2f}')
