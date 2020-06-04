a7fbc717b6e1262f8e6406e064b76c8921f4a2b4
LonwaboMvovo <44nator@gmail.com>
2020-06-04 12:12:20 +0200

    changed order of code

--- main.py
+++ main.py
@@ -6,10 +6,12 @@ from math import ceil
 
 
 def rounder_upper(base_num):
+    '''returns the given number rounded to the chosen number'''
     return round_to * ceil(base_num/round_to)
 
 
 def money_display(amount):
+    '''returns a string formated to how money is displayed'''
     return f'{currency_symbols[currency]}{round(amount, 2):.2f}'
 
 
@@ -24,23 +26,21 @@ currency_symbols = {
 short = 0
 
 while True:
+        try:
+            currency = input('(R)and / (P)ound / (Y)en / (E)ure / (D)ollar: ').lower()
+            assert currency == 'r' or currency == 'p' or currency == 'y' or currency == 'e' or currency == 'd'
+            break
+        except:
+            print('Invalid input. Please input either (R) for Rand, (P) for Pound, (Y) for Yen, (E) for Euro or (D) for Dollar for the currency.')
+
+while True:
     try:
         num_ppl = int(input('Number of people: '))
         break
     except ValueError:
         print('Invalid input. Please input an integer for the number of people.')
 
-split_even = input('Even split? (Y)es or (N)o: ').lower()
-
-while True:
-    try:
-        currency = input('(R)and / (P)ound / (Y)en / (E)ure / (D)ollar: ').lower()
-        assert currency == 'r' or currency == 'p' or currency == 'y' or currency == 'e' or currency == 'd'
-        break
-    except:
-        print('Invalid input. Please input either (R) for Rand, (P) for Pound, (Y) for Yen, (E) for Euro or (D) for Dollar for the currency.')
-
-if split_even == 'y':
+if num_ppl == 1:
     while True:
         try:
             bill = float(input(f'Bill: {currency_symbols[currency]}'))
@@ -70,44 +70,30 @@ if split_even == 'y':
                 print('Invalid input. Please input either 1, 5 or 10 for the number you would like your total to be rounded to.')
         total = rounder_upper(total)
 
-    if num_ppl > 1:
-        tip_pp = tip / num_ppl
-        total_pp = total / num_ppl
-
-        if round_up == 'y':
-            total_pp = rounder_upper(total_pp)
-
-        print('Tip per person:', money_display(tip_pp))
-        print('Total per person:', money_display(total_pp))
-
-        short = round(total, 2) - round(total_pp, 2) * 3
-
-        if short < 0:
-            tip = tip_pp * num_ppl
-            total = total_pp * num_ppl
-
-    print('Tip:', money_display(tip))
+    print('\nTip total:', money_display(tip))
     print('Total:', money_display(total))
-
-    if round(short, 2) > 0:
-        print('Short:', money_display(short))
 else:
-    ppl_tips = []
-    ppl_totals = []
-    for person in range(num_ppl):
-        person_bill = float(input(f'Person-{person + 1} bill: {currency_symbols[currency]}'))
+    split_even = input('Even split? (Y)es or (N)o: ').lower()
+
+    if split_even == 'y':
+        while True:
+            try:
+                bill = float(input(f'Bill: {currency_symbols[currency]}'))
+                break
+            except ValueError:
+                print('Invalid input. Please input a float for the bill.')
 
         while True:
             try:
-                person_tip_perc = float(input('Tip percentage: '))
+                tip_perc = float(input('Tip percentage: '))
                 break
             except ValueError:
                 print('Invalid input. Please input a float for the tip percentage.')
 
         round_up = input('Round up? (Y)es or (N)o: ').lower()
 
-        person_tip = person_bill * (person_tip_perc/100)
-        person_total = person_bill + person_tip
+        tip = bill * (tip_perc/100)
+        total = bill + tip
 
         if round_up == 'y':
             while True:
@@ -117,17 +103,65 @@ else:
                     break
                 except:
                     print('Invalid input. Please input either 1, 5 or 10 for the number you would like your total to be rounded to.')
-            person_total = rounder_upper(person_total)
+            total = rounder_upper(total)
 
-        ppl_tips.append(person_tip)
-        ppl_totals.append(person_total)
+        if num_ppl > 1:
+            tip_pp = tip / num_ppl
+            total_pp = total / num_ppl
 
-    for i in range(num_ppl):
-        print(f'Person-{i + 1} tip: {currency_symbols[currency]}{round(ppl_tips[i], 2):.2f}')
-        print(f'Person-{i + 1} total: {round(ppl_totals[i], 2):.2f}')
+            if round_up == 'y':
+                total_pp = rounder_upper(total_pp)
 
-    tip = sum(ppl_tips)
-    total = sum(ppl_totals)
+            print('\nTip per person:', money_display(tip_pp))
+            print('Total per person:', money_display(total_pp))
 
-    print(f'Tip: {currency_symbols[currency]}{round(tip, 2):.2f}')
-    print(f'Total: {currency_symbols[currency]}{round(total, 2):.2f}')
+            short = round(total, 2) - round(total_pp, 2) * 3
+
+            if short < 0:
+                tip = tip_pp * num_ppl
+                total = total_pp * num_ppl
+
+        print('\nTip total:', money_display(tip))
+        print('Total:', money_display(total))
+
+        if round(short, 2) > 0:
+            print('\nShort:', money_display(short))
+    else:
+        ppl_tips = []
+        ppl_totals = []
+        for person in range(num_ppl):
+            person_bill = float(input(f'Person-{person + 1} bill: {currency_symbols[currency]}'))
+
+            while True:
+                try:
+                    person_tip_perc = float(input('Tip percentage: '))
+                    break
+                except ValueError:
+                    print('Invalid input. Please input a float for the tip percentage.')
+
+            round_up = input('Round up? (Y)es or (N)o: ').lower()
+
+            person_tip = person_bill * (person_tip_perc/100)
+            person_total = person_bill + person_tip
+
+            if round_up == 'y':
+                while True:
+                    try:
+                        round_to = int(input('Round to nearest (1), (5), (10): '))
+                        assert round_to == 1 or round_to == 5 or round_to == 10
+                        break
+                    except:
+                        print('Invalid input. Please input either 1, 5 or 10 for the number you would like your total to be rounded to.')
+                person_total = rounder_upper(person_total)
+
+            ppl_tips.append(person_tip)
+            ppl_totals.append(person_total)
+
+        for i in range(num_ppl):
+            print(f'\nPerson-{i + 1}:\nTip: {money_display(ppl_tips[i])}\nTotal: {money_display(ppl_totals[i])}')
+
+        tip = sum(ppl_tips)
+        total = sum(ppl_totals)
+
+        print('\nTip total:', money_display(tip))
+        print('Total:', money_display(total))
